3d6c340f8543d249e59e2ab5a7c143abeac9e54f
Adzumr <114644108+Adzumr-flyweis@users.noreply.github.com>
2022-10-14 11:47:35 +0100

    created models

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,30 +1,30 @@
-// class CartModel {
-//   final String? product;
-//   final int? price;
-//   final String? photo;
-//   final double? ratings;
-//   final int? quanity;
+class CartModel {
+  final String? product;
+  final int? price;
+  final String? photo;
+  final double? ratings;
+  final int? quanity;
 
-//   CartModel({
-//     this.product,
-//     this.price,
-//     this.photo,
-//     this.ratings,
-//     this.quanity,
-//   });
+  CartModel({
+    this.product,
+    this.price,
+    this.photo,
+    this.ratings,
+    this.quanity,
+  });
 
-//   CartModel.fromJson(Map<String, dynamic> json)
-//       : product = json['Product'] as String?,
-//         price = json['Price'] as int?,
-//         photo = json['Photo'] as String?,
-//         ratings = json['Ratings'] as double?,
-//         quanity = json['quanity'] as int?;
+  CartModel.fromJson(Map<String, dynamic> json)
+      : product = json['Product'] as String?,
+        price = json['Price'] as int?,
+        photo = json['Photo'] as String?,
+        ratings = json['Ratings'] as double?,
+        quanity = json['quanity'] as int?;
 
-//   Map<String, dynamic> toJson() => {
-//         'Product': product,
-//         'Price': price,
-//         'Photo': photo,
-//         'Ratings': ratings,
-//         'quanity': quanity
-//       };
-// }
+  Map<String, dynamic> toJson() => {
+        'Product': product,
+        'Price': price,
+        'Photo': photo,
+        'Ratings': ratings,
+        'quanity': quanity
+      };
+}
